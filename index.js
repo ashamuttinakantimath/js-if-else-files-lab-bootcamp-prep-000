9823180fbeccd9d2a949c21a804471cf4b56c282
asha muttinakntimath <asha.mumath@gmail.com>
2017-10-03 07:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+console.log("you got both height and name")
 if(name==="susan"){
   console.log("incorrect name")
 }
 else{
   console.log("ssssss")
 }
-var index = require("./index.js")
+var index = require("./index.js")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
